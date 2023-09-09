925aec12250b06e40e584a3cddcd16f3e46794e9
rajes <rajes@Rakesh>
2023-09-09 22:56:26 +0530

    "payement bug fixces"

---
+++ ProjectG/src/logicalProgram/Sample1.java
@@ -0,0 +1,25 @@
+package logicalProgram;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		int no=11;
+		 int count=0;
+		for(int i=2;i<no-1;i++)
+		{		if(no%i==0)
+			{
+				count=count+1;
+			}
+		}
+		if(count>0)
+		{
+			System.out.println("no is not prime");
+		}
+		else
+		{
+			System.out.println("no is prime");
+		}
+		
+		
+	}
+
+}
